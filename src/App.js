1d4d1e10564d09b73f278b4b0a97153c08ee4044
absara428 <saraabidi428@gmail.com>
2022-07-06 11:14:28 +0100

    routes created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Header from "./components/Header";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <BrowserRouter>
+      {/*gonna wrap the whole webapp inside these router tags */}
       <Header />
+      <div>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/cart" exact>
+          <Cart />
+        </Route>
+      </div>
     </BrowserRouter>
   );
 }
