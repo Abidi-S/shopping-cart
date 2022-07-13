48b4307e0e72ca1645eccaa8f76bff0fb9d12c06
absara428 <saraabidi428@gmail.com>
2022-07-14 00:13:01 +0100

    fake data generated using Falso

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,8 +1,29 @@
 import React, { createContext } from "react";
+import {
+  rand,
+  randBoolean,
+  randImg,
+  randNumber,
+  randProductName,
+  randUuid,
+  seed,
+} from "@ngneat/falso";
 
 const CartContext = createContext();
 
+seed(214831);
+
 const Context = ({ children }) => {
+  const products = [...Array(20)].map(() => ({
+    id: randUuid(),
+    name: randProductName(),
+    price: randNumber({ length: 3 }),
+    image: randImg(),
+    inStock: rand([0, 3, 5, 6, 7]),
+    fastDelivery: randBoolean(),
+    ratings: rand([1, 2, 3, 4, 5]),
+  }));
+  console.log(products);
   return <CartContext.Provider>{children}</CartContext.Provider>;
 };
 
