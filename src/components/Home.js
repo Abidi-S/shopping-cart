1c52ec2c411c142140afff8572dc65d5a9ea4eb9
absara428 <saraabidi428@gmail.com>
2022-07-15 00:00:01 +0100

    Home UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
 import { CartState } from "../context/Context";
+import SingleProduct from "./SingleProduct";
+import "./styles.css";
 
 const Home = () => {
-  CartState();
-  return <div>Home</div>;
+  const {
+    state: { products },
+  } = CartState();
+
+  return (
+    <div className="home">
+      {/* <Filters /> */}
+      <div className="productContainer">
+        {products.map((prod) => {
+          return <SingleProduct prod={prod} key={prod.id} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
