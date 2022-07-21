02f5c88d321138639bb1dc2740930dd5068a5b2b
absara428 <saraabidi428@gmail.com>
2022-07-21 22:16:15 +0100

    Filter component completed, excluding Rating component

---
+++ src/components/Filters.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import "./styles.css";
+
+const Filters = () => {
+  return (
+    <div className="filters">
+      <span className="title">Filter Products</span>
+      <span>
+        <Form.Check
+          inline
+          label="Ascending"
+          name="group1"
+          type="radio"
+          id={`inline-1`}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="Descending"
+          name="group1"
+          type="radio"
+          id={`inline-2`}
+        />
+      </span>
+      <Button variant="light">Clear Filters</Button>
+    </div>
+  );
+};
+
+export default Filters;
